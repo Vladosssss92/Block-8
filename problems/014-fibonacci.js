b52ab087a7715b7694bf1c3ccfd2b58f2ab627b8
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-05 19:09:57 +0300

    decided-014 (learn JS...) (006 / 010 problem)

--- problems/014-fibonacci.js
+++ problems/014-fibonacci.js
@@ -17,7 +17,16 @@
  * @returns {number}
  */
 function fibonacci(n) {
-    return undefined;
+    if (n === 0) return 0;
+    let a = 0;
+    let b = 1;
+    let c = a + b;
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 }
 
 module.exports = fibonacci;
